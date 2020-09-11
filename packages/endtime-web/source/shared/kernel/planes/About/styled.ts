a6ffc727bb79aa693cf6e422b24c39eb07699fd9
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-09-11 09:51:10 +0300

    endtime-web > setup: package

--- packages/endtime-web/source/shared/kernel/planes/About/styled.ts
+++ packages/endtime-web/source/shared/kernel/planes/About/styled.ts
@@ -4,5 +4,5 @@ import styled from 'styled-components';
 
 export const StyledAbout = styled.div`
     font-family: 'Ubuntu', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Open Sans', 'Helvetica Neue', sans-serif;
-    height: 500px;
+    padding: 2rem;
 `;
